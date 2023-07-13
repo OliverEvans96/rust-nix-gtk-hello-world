6d99d06f8b3f99bc7a5997e07e23585dbe1a7a3f
Oliver Evans <oliverevans96@gmail.com>
2023-07-13 12:23:25 -0700

    Clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -14,37 +14,21 @@
       fenix-system = fenix.packages.x86_64-linux;
       rust-toolchain =
         (with fenix-system; combine [ default.toolchain complete.rust-src ]);
+
     in flake-utils.lib.eachDefaultSystem (system: {
       defaultPackage = pkgs.hello;
       devShell = pkgs.mkShell {
         name = "rust-env";
         src = ./.;
 
-        # build-time deps
-        # from https://blog.thomasheartman.com/posts/bevy-getting-started-on-nixos
         nativeBuildInputs = (with pkgs; [
           rust-toolchain
           rust-analyzer
 
           glibc
           gtk4
-          # gdk-pixbuf
-
-          # lld
-          # clang
-
-          # cargo-edit
-          # cargo-watch
-
           pkgconfig
-          # udev
         ]);
-
-        # shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
-        #   pkgs.alsaLib
-        #   pkgs.udev
-        #   pkgs.vulkan-loader
-        # ]}"'';
       };
     });
 }
